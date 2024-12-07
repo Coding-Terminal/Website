5b3d83b51621bbae9e92004dd9905a19ce2c3e45
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-12-07 13:52:26 +0530

    feat(tailwind): colors-variable and text combine variables

---
+++ src/Component/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar : React.FC = () => {
+  return (
+    <div className="w-screen h-20  bg-brandprimary">
+       <p className='heading-l'>Coding Terminal</p>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      brandprimary: "var(--color-brandprimary)",
+      brandsecondary: "var(--color-brandsecondary)",
+      brandtertiary: "var(--color-brandtertiary)",
+      brandgeneral: "var(--color-brandgeneral)",
+      textprimary  :"var(--color-textprimary)",
+      textsecondary : "var(--color-textsecondary)",
+      textprimaryinverse:"var(--color-textprimaryinverse)"
+      }
+    },
   },
   plugins: [],
 }
