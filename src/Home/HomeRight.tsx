07673a2839f4eb07fa2f01b9d5c983ccecb28b46
arpitjaingwl7 <arpitjaingwl7@gmail.com>
2024-12-07 12:27:45 +0530

    Typescript configured and add

---
+++ src/Home/HomeRight.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomeRight: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <img
+        src="./assets/Home-right-png.png"
+        alt="Team Collaboration"
+        className="w-full xl:w-4/5 object-contain rounded-lg"
+      />
+    </div>
+  );
+};
+
+export default HomeRight;
